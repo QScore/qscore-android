ff257fc81dcb455b0880c7d93962c56897dca163
Phil Olson <polson55@gmail.com>
2020-03-18 17:15:32 -0500

    Cleanup

--- app/src/main/java/com/berd/qscore/features/geofence/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/berd/qscore/features/geofence/GeofenceBroadcastReceiver.kt
@@ -22,7 +22,6 @@ class GeofenceIntentService : IntentService(GeofenceIntentService.javaClass.simp
     }
 
     override fun onHandleIntent(intent: Intent?) {
-        Timber.d(">>Handling intent: $intent")
         val geofencingEvent = GeofencingEvent.fromIntent(intent)
         if (geofencingEvent.hasError()) {
             val errorMessage = when (geofencingEvent.errorCode) {
@@ -38,11 +37,9 @@ class GeofenceIntentService : IntentService(GeofenceIntentService.javaClass.simp
         // Get the transition type.
         when (geofencingEvent.geofenceTransition) {
             Geofence.GEOFENCE_TRANSITION_ENTER -> {
-                Timber.d(">>User came home")
                 eventSubject.onNext(Event.Entered)
             }
             Geofence.GEOFENCE_TRANSITION_EXIT -> {
-                Timber.d(">>User left home")
                 eventSubject.onNext(Event.Exited)
             }
         }
