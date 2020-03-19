81836ec7174cec33cc6c5588523624cfb6b2081b
Phil Olson <polson55@gmail.com>
2020-03-19 14:29:25 -0500

    Fix amplify

--- app/src/main/java/com/berd/qscore/QscoreApplication.kt
+++ app/src/main/java/com/berd/qscore/QscoreApplication.kt
@@ -1,11 +1,13 @@
 package com.berd.qscore
 
 import android.app.Application
+import android.util.Log
 import com.berd.qscore.features.shared.prefs.Prefs
 import com.berd.qscore.utils.geofence.GeofenceHelper
 import com.berd.qscore.utils.injection.AppInjector
 import com.berd.qscore.utils.injection.Injector
 import com.berd.qscore.utils.logging.LogHelper
+import timber.log.Timber
 
 
 class QscoreApplication : Application() {
@@ -14,7 +16,18 @@ class QscoreApplication : Application() {
         super.onCreate()
         LogHelper.initializeLogging()
         Injector.initialize(AppInjector(this))
+        setupAmplify()
         setupGeofence()
+    }
+
+    private fun setupAmplify() {
+        try {
+            Amplify.addPlugin(AWSApiPlugin())
+            Amplify.configure(applicationContext)
+            Log.i("AmplifyGetStarted", "Amplify is all setup and ready to go!")
+        } catch (exception: AmplifyException) {
+            Timber.d(exception.getMessage())
+        }
     }
 
     private fun setupGeofence() {
