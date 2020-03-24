702bc01b478ed933ee77cfe1674e27684a158412
Kyle Olson <olso4051@umn.edu>
2020-03-24 18:35:17 -0500

    added SignUpActivity

---
+++ app/src/main/java/com/berd/qscore/features/login/SignUpActivity.kt
@@ -0,0 +1,134 @@
+package com.berd.qscore.features.login
+
+import android.app.ProgressDialog
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.berd.qscore.R
+import com.berd.qscore.databinding.ActivityLoginBinding
+import com.berd.qscore.features.login.confirmation.ConfirmActivity
+import com.berd.qscore.features.score.ScoreActivity
+import com.berd.qscore.features.welcome.WelcomeActivity
+import com.berd.qscore.utils.extensions.gone
+import com.berd.qscore.utils.extensions.invisible
+import com.berd.qscore.utils.extensions.showProgressDialog
+import com.berd.qscore.utils.extensions.visible
+import com.facebook.CallbackManager
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import kotlinx.android.synthetic.main.activity_login.*
+import splitties.activities.start
+import timber.log.Timber
+
+class SignUpActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+    private val callbackManager by lazy { CallbackManager.Factory.create() }
+    private val viewModel by viewModels<SignUpViewModel>()
+    private var progressDialog: ProgressDialog? = null
+
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = binding.root
+        setContentView(view)
+        observeEvents()
+        setupViews()
+    }
+
+    private fun observeEvents() {
+        viewModel.actions.subscribeBy(onNext = {
+            handleActions(it)
+        }, onError = {
+            Timber.e("Error subscribing to events: $it")
+        }).addTo(compositeDisposable)
+
+        viewModel.state.observe(this, Observer {
+            when (it) {
+                SignUpViewModel.State.InProgress -> handleInProgress()
+                SignUpViewModel.State.LoginError -> handleLoginError()
+                SignUpViewModel.State.SignupError -> handleSignUpError()
+                SignUpViewModel.State.Ready -> handleReady()
+            }
+        })
+    }
+
+    private fun handleSignUpError() = binding.apply {
+        progressDialog?.dismiss()
+        errorText.text = getString(R.string.sign_up_error)
+        errorText.visible()
+    }
+
+    private fun handleReady() {
+        progressDialog?.dismiss()
+        errorText.gone()
+    }
+
+    private fun handleLoginError() {
+        progressDialog?.dismiss()
+        errorText.text = getString(R.string.login_error)
+        errorText.visible()
+    }
+
+    private fun handleInProgress() {
+        progressDialog = showProgressDialog("Signing in...")
+        errorText.invisible()
+    }
+
+
+
+    private fun handleActions(it: SignUpViewModel.Action) {
+        when (it) {
+            SignUpViewModel.Action.LaunchScoreActivity -> launchScoreActivity()
+            is SignUpViewModel.Action.LaunchConfirmActivity -> launchConfirmActivity(it.email)
+            is SignUpViewModel.Action.LaunchWelcomeActivity -> launchWelcomeActivity()
+        }
+    }
+
+    private fun launchWelcomeActivity() {
+        start<WelcomeActivity>()
+        progressDialog?.dismiss()
+        finish()
+    }
+
+    private fun launchConfirmActivity(email: String) {
+        val intent = ConfirmActivity.newIntent(this, email)
+        progressDialog?.dismiss()
+        startActivity(intent)
+    }
+
+    private fun launchScoreActivity() {
+        start<ScoreActivity>()
+        progressDialog?.dismiss()
+        finish()
+    }
+
+    private fun setupViews() = binding.apply {
+        login.setOnClickListener {
+            val email = email.text.toString()
+            val password = password.text.toString()
+            viewModel.onLogin(email, password)
+        }
+
+        fbLogin.setOnClickListener {
+            viewModel.loginFacebook(supportFragmentManager)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+
+}
---
+++ app/src/main/java/com/berd/qscore/features/login/SignUpViewModel.kt
@@ -0,0 +1,108 @@
+package com.berd.qscore.features.login
+
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.amazonaws.services.cognitoidentityprovider.model.UserNotConfirmedException
+import com.amazonaws.services.cognitoidentityprovider.model.UserNotFoundException
+import com.berd.qscore.features.login.LoginManager.LoginEvent.Success
+import com.berd.qscore.features.login.LoginManager.LoginEvent.Unknown
+import com.berd.qscore.features.login.LoginManager.SignupEvent
+import com.berd.qscore.features.login.SignUpViewModel.Action.*
+import com.berd.qscore.features.login.SignUpViewModel.State.*
+import com.berd.qscore.features.shared.prefs.Prefs
+import com.berd.qscore.utils.rx.RxEventSender
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.launch
+
+class SignUpViewModel : ViewModel() {
+
+    sealed class Action {
+        object LaunchScoreActivity : Action()
+        class LaunchConfirmActivity(val email: String) : Action()
+        object LaunchWelcomeActivity : Action()
+    }
+
+    sealed class State {
+        object InProgress : State()
+        object LoginError : State()
+        object SignupError : State()
+        object Ready : State()
+    }
+
+    private val _actions = RxEventSender<Action>()
+    val actions = _actions.observable
+
+    private val _state = MutableLiveData<State>()
+    val state = _state as LiveData<State>
+
+    fun onLogin(email: String, password: String) = viewModelScope.launch {
+        _state.postValue(InProgress)
+        try {
+            when (LoginManager.login(email, password)) {
+                Success -> handleSuccess()
+                Unknown -> handleUnknown()
+            }
+        } catch (e: Exception) {
+            when (e) {
+                is UserNotConfirmedException -> _actions.send(LaunchConfirmActivity(email))
+                is UserNotFoundException -> signup(email, password)
+                else -> _state.postValue(LoginError)
+            }
+        }
+    }
+
+    private fun signup(email: String, password: String) = viewModelScope.launch {
+        try {
+            when (LoginManager.signUp(email, password)) {
+                SignupEvent.Success -> handleSuccess()
+                is SignupEvent.NeedConfirmation -> handleNeedConfirmation(email)
+            }
+        } catch (e: Exception) {
+            when (e) {
+                is UserNotConfirmedException -> {
+                    _state.postValue(Ready)
+                    _actions.send(LaunchConfirmActivity(email))
+                }
+                else -> _state.postValue(SignupError)
+            }
+        }
+    }
+
+
+    fun loginFacebook(supportFragmentManager: FragmentManager) = viewModelScope.launch {
+        _state.postValue(InProgress)
+        try {
+            when (LoginManager.loginFacebook(supportFragmentManager)) {
+                Success -> handleSuccess()
+                Unknown -> handleUnknown()
+            }
+        } catch (e: Exception) {
+            if (e is CancellationException) {
+                _state.postValue(Ready)
+            } else {
+                _state.postValue(LoginError)
+            }
+        }
+    }
+
+    private fun handleSuccess() {
+        _state.postValue(Ready)
+        if (Prefs.userLocation != null) {
+            _actions.send(LaunchScoreActivity)
+        } else {
+            _actions.send(LaunchWelcomeActivity)
+        }
+    }
+
+    private fun handleUnknown() {
+        _state.postValue(LoginError)
+    }
+
+    private fun handleNeedConfirmation(email: String) {
+        _state.postValue(Ready)
+        _actions.send(LaunchConfirmActivity(email))
+    }
+}
